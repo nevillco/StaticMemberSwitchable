ce4fd4b920de3cbc1b743b218ebe57dec1d04b9e
Connor Neville <connor.neville16@gmail.com>
2023-10-10 18:53:52 -0400

    Clean up implementation with helper funcs.

--- Sources/StaticMemberSwitchableMacro/StaticMemberSwitchableMacro.swift
+++ Sources/StaticMemberSwitchableMacro/StaticMemberSwitchableMacro.swift
@@ -1,3 +1,4 @@
+// MARK: - StaticMemberSwitchableMacro
 public struct StaticMemberSwitchableMacro: MemberMacro {
 
     public static func expansion<Declaration: DeclGroupSyntax, Context: MacroExpansionContext>(
@@ -6,14 +7,54 @@ public struct StaticMemberSwitchableMacro: MemberMacro {
         in context: Context
     ) throws -> [DeclSyntax] {
         guard let structDeclaration = declaration.as(StructDeclSyntax.self) else {
-            throw StaticMemberSwitchableError.notAStruct.diagnostic(node: node)
+            throw StaticMemberSwitchableError
+                .notAStruct.diagnostic(node: node)
         }
+
+        let staticPropertyIdentifiers = validStaticProperties(
+            declaration: structDeclaration
+        ).map(\.identifier.text)
+
+        let conformanceSpecificInfo = try conformanceSpecificInfo(
+            declaration: structDeclaration,
+            node: node
+        )
+
+        let cases = staticPropertyIdentifiers
+            .map { "case \($0)" }
+            .joined(separator: "\n")
+        return [
+            """
+            enum StaticMemberSwitchable {
+                \(raw: cases)
+            }
+            var switchable: StaticMemberSwitchable {
+                switch \(raw: conformanceSpecificInfo.switchValue) {
+                    \(raw: staticPropertyIdentifiers
+                    .map(conformanceSpecificInfo.makeCaseFromPropertyName)
+                    .joined(separator: "\n        ")
+                    )
+                    default: fatalError()
+                }
+            }
+            """
+        ]
+    }
+
+}
+
+// MARK: - StaticMemberSwitchableMacro - Private
+private extension StaticMemberSwitchableMacro {
+
+    static func validStaticProperties(
+        declaration: StructDeclSyntax
+    ) -> [VariableDeclSyntax] {
         // Only include static members with the same type as the declaration’s type.
         let validStaticPropertyTypeNames: Set<String> = [
-            structDeclaration.name.trimmedDescription,
+            declaration.name.trimmedDescription,
             "Self"
         ]
-        let staticProperties = declaration.properties.filter { property in
+        return declaration.properties.filter { property in
             let possibleSelfReferencesArray = [
                 // static let ___: Self = ...
                 property.bindings
@@ -47,49 +88,49 @@ public struct StaticMemberSwitchableMacro: MemberMacro {
             && property.accessLevel >= declaration.declAccessLevel
             && property.isStatic
         }
-        let staticPropertyIdentifiers = staticProperties.map(\.identifier.text)
+    }
 
-        let inheritance = structDeclaration.inheritanceClause?.inheritedTypes ?? []
-        let declarationInheritsProtocol: (String) -> Bool = { protocolName in
-            inheritance.contains { $0.trimmed.description == protocolName }
-        }
+    static func declaration(
+        _ declaration: StructDeclSyntax,
+        inheritsProtocolNamed protocolName: String
+    ) -> Bool {
+        let inheritedProtocols = declaration.inheritanceClause?.inheritedTypes
+            .map(\.trimmedDescription) ?? []
+        return inheritedProtocols.contains(protocolName)
+    }
 
+    struct ConformanceSpecificInfo {
         let switchValue: String
-        let switchCaseForStaticProperty: (String) -> String
-        if declarationInheritsProtocol("Identifiable") {
-            switchValue = "id"
-            switchCaseForStaticProperty = { staticProperty in
-                "case Self.\(staticProperty).id: return .\(staticProperty)"
-            }
-        } else if declarationInheritsProtocol("Equatable") {
-            switchValue = "self"
-            switchCaseForStaticProperty = { staticProperty in
-                "case .\(staticProperty): return .\(staticProperty)"
-            }
+        let makeCaseFromPropertyName: (String) -> String
+    }
+    static func conformanceSpecificInfo(
+        declaration: StructDeclSyntax,
+        node: AttributeSyntax
+    ) throws -> ConformanceSpecificInfo {
+        if self.declaration(
+            declaration,
+            inheritsProtocolNamed: "Identifiable"
+        ) {
+            return .init(
+                switchValue: "id",
+                makeCaseFromPropertyName: { propertyName in
+                    "case Self.\(propertyName).id: return .\(propertyName)"
+                }
+            )
+        } else if self.declaration(
+            declaration,
+            inheritsProtocolNamed: "Equatable"
+        ) {
+            return .init(
+                switchValue: "self",
+                makeCaseFromPropertyName: { propertyName in
+                    "case .\(propertyName): return .\(propertyName)"
+                }
+            )
         } else {
             throw StaticMemberSwitchableError.missingRequiredConformance
                 .diagnostic(node: node)
         }
-
-        let cases = staticPropertyIdentifiers
-            .map { "case \($0)" }
-            .joined(separator: "\n")
-        return [
-            """
-            enum StaticMemberSwitchable {
-                \(raw: cases)
-            }
-            var switchable: StaticMemberSwitchable {
-                switch \(raw: switchValue) {
-                    \(raw: staticPropertyIdentifiers
-                    .map(switchCaseForStaticProperty)
-                    .joined(separator: "\n        ")
-                    )
-                    default: fatalError()
-                }
-            }
-            """
-        ]
     }
 
 }
