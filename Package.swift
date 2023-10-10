d6722bf9ecc77aa604d0107e6fd6e991c41e9fcc
Connor Neville <connor.neville16@gmail.com>
2023-10-10 14:04:04 -0400

    Initial commit: run swift package init; configure Package.swift structure; add ReplaceMe.swift source files.

---
+++ Package.swift
@@ -0,0 +1,53 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import CompilerPluginSupport
+import PackageDescription
+
+let package = Package(
+    name: "StaticMemberSwitchable",
+    platforms: [.macOS(.v10_15), .iOS(.v13)],
+    products: [
+        .library(
+            name: "StaticMemberSwitchable",
+            targets: ["StaticMemberSwitchable"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-syntax.git",
+            from: "509.0.0-swift-5.9-DEVELOPMENT-SNAPSHOT-2023-04-25-b"
+        ),
+    ],
+    targets: [
+        // Actual macro implementation that performs the compile-time codegen.
+        .macro(
+            name: "StaticMemberSwitchableMacro",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
+        // Library that exposes a macro as part of its API, which is used in client programs.
+        .target(
+            name: "StaticMemberSwitchable",
+            dependencies: ["StaticMemberSwitchableMacro"]
+        ),
+        // An example client to demonstrate usage.
+        .executableTarget(
+            name: "StaticMemberSwitchableExampleClient",
+            dependencies: ["StaticMemberSwitchable"]
+        ),
+        // A test target used to develop the macro implementation.
+        .testTarget(
+            name: "StaticMemberSwitchableMacroTests",
+            dependencies: [
+                "StaticMemberSwitchableMacro",
+                .product(
+                    name: "SwiftSyntaxMacrosTestSupport",
+                    package: "swift-syntax"
+                ),
+            ]
+        ),
+    ]
+)
